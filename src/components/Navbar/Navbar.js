2e4f8f4ae27a4d3a5f2b5bb23a24f347cc2f6184
4SAMU <samuelnzomo4@gmail.com>
2022-10-11 16:13:41 +0300

    first commit

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,26 +3,55 @@
 import React, { useEffect, useState } from "react";
 import { connectWallet } from "../../utils/getAccounts";
 import "./Navbar.css";
+import Drawer from "react-modern-drawer";
+import { ImMenu } from "@react-icons/all-files/im/ImMenu";
+
+//import styles 👇
+import "react-modern-drawer/dist/index.css";
 
 const Navbar = () => {
-  const [address, setAddress]=useState()
+  const [address, setAddress] = useState();
 
+  useEffect(() => {
+    async function fetchData() {
+      const items = await connectWallet();
+      setAddress(
+        String(items.address).substring(0, 5) +
+          "..." +
+          String(items.address).substring(40)
+      );
+    }
+    fetchData();
+  }, []);
 
-  useEffect( () => {
-     async function fetchData() {
-       const items = await connectWallet();
-       setAddress(
-         String(items.address).substring(0, 5) +
-           "..." +
-           String(items.address).substring(40)
-       );
-     }
-     fetchData();
-  },[])
+  const [isOpen, setIsOpen] = React.useState(false);
+  const toggleDrawer = () => {
+    setIsOpen((prevState) => !prevState);
+  };
 
   return (
     <div className="Top_bar">
       <div className="logo">THE_NFTS</div>
+      <button className="menu" onClick={toggleDrawer}>
+        <ImMenu />
+      </button>
+      <div>
+        <Drawer
+          open={isOpen}
+          onClose={toggleDrawer}
+          direction="left"
+          className="drawerBar"
+        >
+          <button>Hello World</button>
+          <br />
+          <button>Hello World</button>
+          <br />
+          <button>Hello World</button>
+          <br />
+          <button>Hello World</button>
+        </Drawer>
+      </div>
+
       {!address ? (
         <button className="connectwalletBTN" onClick={connectWallet}>
           connect Wallet
